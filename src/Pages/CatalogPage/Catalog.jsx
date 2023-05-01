1996e2122d1a6ec80b01f9afe1d664c9576aa40e
DorE420 <117580165+DorE420@users.noreply.github.com>
2023-05-01 19:24:15 +0300

    catalog add

--- src/Pages/CatalogPage/Catalog.jsx
+++ src/Pages/CatalogPage/Catalog.jsx
@@ -2,19 +2,52 @@ import React from "react";
 import ModeIcon from '@mui/icons-material/Mode';
 import "./CatalogCss.css";
 
+const imageContext = require.context('../../images', true, /\.(jpg|jpeg|png|gif)$/);
+const getImages = () => {
+  const images = imageContext.keys().map(imageContext);
+  return images;
+};
+console.log(imageContext.keys());
 const Catalog = () => {
-  return (
-    <div id="mainBodyCatalog">
+  const plantImages = getImages();
+  console.log(plantImages);
+  const plants = [
+    { id: 1, name: 'במבוק נמוך 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high1.jpeg' },
+    { id: 2, name: 'דקל טבעות 25 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high2.jpeg' },
+    { id: 3, name: 'סיזיגיום ספירלה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high3.jpeg' },
+    { id: 4, name: 'מילנבקיה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high4.jpeg' },
+    { id: 5, name: 'קנציה 25 ליטר', category: 'צמחייה בינונית',imageUrl: '/images/mid1.jpeg' },
+    { id: 6, name: 'רימון 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid2.jpeg' },
+    { id: 7, name: 'מילנבקיה משתרע 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid3.jpeg' },
+    { id: 8, name: 'זנב דג 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid4.jpeg' },
+    { id: 9, name: 'מנגבה 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small1.jpeg' },
+    { id: 10, name: 'מרווה רפואית 7 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small2.jpeg' },
+    { id: 11, name: 'הדס כדור 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small3.jpeg' },
+    { id: 12, name: 'לוונדר מנוצה 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small4.jpeg' },
+];
+const groupedPlants = plants.reduce((acc, plant) => {
+  acc[plant.category] = acc[plant.category] || [];
+  acc[plant.category].push(plant);
+  return acc;
+}, {});
 
-      <div id="headerCatalog">
-      <button className="buttonCatalog">הוספת פריט</button>
-        <h1>קטלוג</h1>
+return (
+  <div className="plant-catalog">
+    {Object.entries(groupedPlants).map(([category, categoryPlants]) => (
+      <div key={category} className={`plant-category ${category}`}>
+        <h2>{category.charAt(0).toUpperCase() + category.slice(1)} </h2>
+        <div className="plants">
+          {categoryPlants.map(plant => (
+            <div key={plant.id} className="plant-card">
+              <img src={plant.imageUrl} alt={plant.name} />
+              <h3>{plant.name}</h3>
+            </div>
+          ))}
+        </div>
       </div>
-
-
-      <div id="innerMainCatalog"></div>
-    </div>
-  );
+    ))}
+  </div>
+);
 };
 
 export default Catalog;
