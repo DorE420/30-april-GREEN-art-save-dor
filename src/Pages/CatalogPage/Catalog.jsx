627ab5f504bb0a6e8320a57465e227c32dd89144
DorE420 <117580165+DorE420@users.noreply.github.com>
2023-05-01 19:38:33 +0300

    push last update 19:38

--- src/Pages/CatalogPage/Catalog.jsx
+++ src/Pages/CatalogPage/Catalog.jsx
@@ -12,18 +12,18 @@ const Catalog = () => {
   const plantImages = getImages();
   console.log(plantImages);
   const plants = [
-    { id: 1, name: 'במבוק נמוך 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high1.jpeg' },
-    { id: 2, name: 'דקל טבעות 25 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high2.jpeg' },
-    { id: 3, name: 'סיזיגיום ספירלה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high3.jpeg' },
-    { id: 4, name: 'מילנבקיה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: '/images/high4.jpeg' },
-    { id: 5, name: 'קנציה 25 ליטר', category: 'צמחייה בינונית',imageUrl: '/images/mid1.jpeg' },
-    { id: 6, name: 'רימון 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid2.jpeg' },
-    { id: 7, name: 'מילנבקיה משתרע 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid3.jpeg' },
-    { id: 8, name: 'זנב דג 25 ליטר',category: 'צמחייה בינונית', imageUrl: '/images/mid4.jpeg' },
-    { id: 9, name: 'מנגבה 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small1.jpeg' },
-    { id: 10, name: 'מרווה רפואית 7 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small2.jpeg' },
-    { id: 11, name: 'הדס כדור 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small3.jpeg' },
-    { id: 12, name: 'לוונדר מנוצה 10 ליטר',category: 'צמחייה קטנה', imageUrl: '/images/small4.jpeg' },
+    { id: 1, name: 'במבוק נמוך 50 ליטר',category: 'צמחייה גבוהה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/high1.jpeg' },
+    { id: 2, name: 'דקל טבעות 25 ליטר',category: 'צמחייה גבוהה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/high2.jpeg' },
+    { id: 3, name: 'סיזיגיום ספירלה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/high3.jpeg' },
+    { id: 4, name: 'מילנבקיה 50 ליטר',category: 'צמחייה גבוהה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/high4.jpeg' },
+    { id: 5, name: 'קנציה 25 ליטר', category: 'צמחייה בינונית',imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/mid1.jpeg' },
+    { id: 6, name: 'רימון 25 ליטר',category: 'צמחייה בינונית', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/mid2.jpeg' },
+    { id: 7, name: 'מילנבקיה משתרע 25 ליטר',category: 'צמחייה בינונית', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/mid3.jpeg' },
+    { id: 8, name: 'זנב דג 25 ליטר',category: 'צמחייה בינונית', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/mid4.jpeg' },
+    { id: 9, name: 'מנגבה 10 ליטר',category: 'צמחייה קטנה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/small1.jpeg' },
+    { id: 10, name: 'מרווה רפואית 7 ליטר',category: 'צמחייה קטנה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/small2.jpeg' },
+    { id: 11, name: 'הדס כדור 10 ליטר',category: 'צמחייה קטנה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/small3.jpeg' },
+    { id: 12, name: 'לוונדר מנוצה 10 ליטר',category: 'צמחייה קטנה', imageUrl: 'https://proj.ruppin.ac.il/cgroup96/prod/build/images/small4.jpeg' },
 ];
 const groupedPlants = plants.reduce((acc, plant) => {
   acc[plant.category] = acc[plant.category] || [];
